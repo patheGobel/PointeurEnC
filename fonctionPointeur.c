803d1760d29d9c246b242412080718232511d72f
patheGobel <pathegba@gmail.com>
2024-08-22 21:46:21 +0000

    Je suis en train de d'essayer de trouver une exliquation parfaitement du processus de la déclaration de d'une variable, d'un pointeur puis l'association des deux

--- fonctionPointeur.c
+++ fonctionPointeur.c
@@ -6,7 +6,16 @@ int calculer(int *a, int *b);
 
 int main()
 {
-    int n, m;
+    int n=0, m=0;
+    /*
+    
+    Décrivons de façon imagez ce qui se passe.
+    Qaund vous déclarez la varaible n, elle se trouve à une adresse donnée, par exemple 12543554566.
+    Quand vous déclarez le pinteur *ptr1 = Null, c'est comme si vous avez créer une boite
+    qui porte le nom ptr1 se trouvant à l'adresse 12543554566.
+    Ainsi, ptr1=12543554566.
+
+    */
     int *ptr1 = &n, *ptr2 = &m;
     printf("Donner deux nombres\n");
     scanf("%d%d", &n, &m);
